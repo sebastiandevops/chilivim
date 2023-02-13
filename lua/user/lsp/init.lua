e656440ab19f3c5fcdfb5d635eb7fca4912c9bf8
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-13 11:31:31 -0500

    Add ftplugin for define tabstop for lua files and function to show lsp server on lualine

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -6,5 +6,3 @@ end
 require "user.lsp.mason"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
--- require'lspconfig'.jedi_language_server.setup{}
--- require'lspconfig'.lua_ls.setup{}
--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -58,6 +58,36 @@ end
 -- 	return chars[index]
 -- end
 
+local lsp_server = function(msg)
+  local buf_clients = vim.lsp.buf_get_clients()
+  if next(buf_clients) == nil then
+    -- TODO: clean up this if statement
+    if type(msg) == "boolean" or #msg == 0 then
+      return "LS Inactive"
+    end
+    return msg
+  end
+  local buf_client_names = {}
+  local copilot_active = false
+
+  -- add client
+  for _, client in pairs(buf_clients) do
+    if client.name ~= "null-ls" and client.name ~= "copilot" then
+      table.insert(buf_client_names, client.name)
+    end
+
+    if client.name == "copilot" then
+      copilot_active = true
+    end
+  end
+  local unique_client_names = vim.fn.uniq(buf_client_names)
+  local language_servers = "[" .. table.concat(unique_client_names, ", ") .. "]"
+  if copilot_active then
+    language_servers = language_servers .. "%#SLCopilot#"
+  end
+  return language_servers
+end
+
 local spaces = function()
 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
@@ -122,10 +152,10 @@ lualine.setup({
 	},
 	sections = {
 		lualine_a = { 'branch', 'diagnostics' },
-		lualine_b = { 'mode' },
+		lualine_b = { 'mode', 'diff'},
 		lualine_c = {},
 		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { 'diff', spaces, 'filetype' },
+		lualine_x = { lsp_server, spaces, 'filetype' },
 		lualine_y = { 'location' },
 		lualine_z = { 'progress' },
 	},
