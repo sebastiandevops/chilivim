8b244bd76968a0c861c78a7f85e2079656bfaa0c
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-12 20:04:56 -0500

    Lualine update

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -3,50 +3,50 @@ if not status_ok then
 	return
 end
 
-local hide_in_width = function()
-	return vim.fn.winwidth(0) > 80
-end
+-- local hide_in_width = function()
+-- 	return vim.fn.winwidth(0) > 80
+-- end
 
-local diagnostics = {
-	"diagnostics",
-	sources = { "nvim_diagnostic" },
-	sections = { "error", "warn" },
-	symbols = { error = " ", warn = " " },
-	colored = false,
-	update_in_insert = false,
-	always_visible = true,
-}
+-- local diagnostics = {
+-- 	"diagnostics",
+-- 	sources = { "nvim_diagnostic" },
+-- 	sections = { "error", "warn" },
+-- 	symbols = { error = " ", warn = " " },
+-- 	colored = false,
+-- 	update_in_insert = false,
+-- 	always_visible = true,
+-- }
+--
+-- local diff = {
+-- 	"diff",
+-- 	colored = false,
+-- 	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+--   cond = hide_in_width
+-- }
 
-local diff = {
-	"diff",
-	colored = false,
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width
-}
-
-local mode = {
-	"mode",
-	fmt = function(str)
-		return "-- " .. str .. " --"
-	end,
-}
-
-local filetype = {
-	"filetype",
-	icons_enabled = false,
-	icon = nil,
-}
-
-local branch = {
-	"branch",
-	icons_enabled = true,
-	icon = "",
-}
-
-local location = {
-	"location",
-	padding = 0,
-}
+-- local mode = {
+-- 	"mode",
+-- 	fmt = function(str)
+-- 		return "-- " .. str .. " --"
+-- 	end,
+-- }
+--
+-- local filetype = {
+-- 	"filetype",
+-- 	icons_enabled = false,
+-- 	icon = nil,
+-- }
+--
+-- local branch = {
+-- 	"branch",
+-- 	icons_enabled = true,
+-- 	icon = "",
+-- }
+--
+-- local location = {
+-- 	"location",
+-- 	padding = 0,
+-- }
 
 -- cool function for progress
 -- local progress = function()
@@ -121,12 +121,12 @@ lualine.setup({
 		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
+		lualine_a = { 'branch', 'diagnostics' },
+		lualine_b = { 'mode' },
 		lualine_c = {},
 		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, spaces, "encoding", filetype },
-		lualine_y = { location },
+		lualine_x = { 'diff', spaces, 'filetype' },
+		lualine_y = { 'location' },
 		lualine_z = { 'progress' },
 	},
 	inactive_sections = {
