00cf7b6728ba0853dc75fe4bd9757e3075b168c4
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-14 06:28:41 -0500

    Autocommands, comments keymaps, toogleterm and whick key update to LunarVim/nvim-basic-ide

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -35,24 +35,10 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Move text up and down
--- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
--- keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-
--- Insert --
--- Press jk fast to exit insert mode 
--- keymap("i", "jk", "<ESC>", opts)
--- keymap("i", "kj", "<ESC>", opts)
-
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
-
--- Move text up and down
--- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
--- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
--- keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
@@ -66,12 +52,7 @@ keymap("n", "<TAB>",   ":bnext<CR>", opts)
 keymap("n", "<S-TAB>", ":bprevious<CR>", opts)
 keymap("n", "<S-h>",   ":bprevious<CR>", opts)
 keymap("n", "<S-l>",   ":bnext<CR>", opts)
--- Terminal --
--- Better terminal navigation
--- keymap("n", [[<C-\>]], ":ToggleTerm<CR>", opts)
 
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
+-- Comment
+keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+keymap("x", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,4 +1,3 @@
--- Config {{{1
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,20 +10,8 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
--- }}}1
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 require('lazy').setup('plugins')
-  -- }}}1
--- Plugins Config {{{1
-vim.keymap.set({ 'n' }, '<C-k>', function()       require('lsp_signature').toggle_float_win()
-end, { silent = true, noremap = true, desc = 'toggle signature' })
-
-vim.keymap.set({ 'n' }, '<Leader>k', function()
- vim.lsp.buf.signature_help()
-end, { silent = true, noremap = true, desc = 'toggle signature' })
-
-
--- }}}1
-
