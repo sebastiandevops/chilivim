b0ec09c1720b013d477538d0c902a18338ca5f22
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-14 06:53:12 -0500

    lualine update to new repo

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -3,61 +3,6 @@ if not status_ok then
 	return
 end
 
--- local hide_in_width = function()
--- 	return vim.fn.winwidth(0) > 80
--- end
-
--- local diagnostics = {
--- 	"diagnostics",
--- 	sources = { "nvim_diagnostic" },
--- 	sections = { "error", "warn" },
--- 	symbols = { error = " ", warn = " " },
--- 	colored = false,
--- 	update_in_insert = false,
--- 	always_visible = true,
--- }
---
--- local diff = {
--- 	"diff",
--- 	colored = false,
--- 	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
---   cond = hide_in_width
--- }
-
--- local mode = {
--- 	"mode",
--- 	fmt = function(str)
--- 		return "-- " .. str .. " --"
--- 	end,
--- }
---
--- local filetype = {
--- 	"filetype",
--- 	icons_enabled = false,
--- 	icon = nil,
--- }
---
--- local branch = {
--- 	"branch",
--- 	icons_enabled = true,
--- 	icon = "",
--- }
---
--- local location = {
--- 	"location",
--- 	padding = 0,
--- }
-
--- cool function for progress
--- local progress = function()
--- 	local current_line = vim.fn.line(".")
--- 	local total_lines = vim.fn.line("$")
--- 	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
--- 	local line_ratio = current_line / total_lines
--- 	local index = math.ceil(line_ratio * #chars)
--- 	return chars[index]
--- end
-
 local lsp_server = function(msg)
   local buf_clients = vim.lsp.buf_get_clients()
   if next(buf_clients) == nil then
@@ -86,6 +31,7 @@ end
 
 lualine.setup({
 	options = {
+    globalstatuses = true,
 		icons_enabled = true,
 		theme = {
       normal = {
@@ -139,7 +85,7 @@ lualine.setup({
     },
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		disabled_filetypes = { "alpha", "dashboard" },
 		always_divide_middle = true,
 	},
 	sections = {
