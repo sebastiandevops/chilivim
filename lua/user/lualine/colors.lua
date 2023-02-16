591502852f98caca293dad9aeb579cb21c326388
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-16 08:39:24 -0500

    Modularized lualine colors

---
+++ lua/user/lualine/colors.lua
@@ -0,0 +1,10 @@
+local colors = {
+  grey_medium = '#908CA9',
+  grey = '#DFDDF3',
+  error_red = '#0000ffff',
+  pine = '#286983',
+  grey_deep = '#6E6A86',
+  iris = '#907AA8',
+  red_love = '#B3637A',
+}
+return colors
