d37e8c64dedc974912337e83dc1f9c5d26ba9c13
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-17 13:34:04 -0500

    Enable python for treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -33,7 +33,7 @@ configs.setup({
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python", "css" } },
+	indent = { enable = true, disable = { "css" } },
   playground = {
     enable = true,
     disable = {},
