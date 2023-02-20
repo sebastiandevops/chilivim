469976a316ef47a3b3307776666de5025e1f33df
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-20 08:57:03 -0500

    Updated components to improve modularization

--- lua/user/lualine/components.lua
+++ lua/user/lualine/components.lua
@@ -14,6 +14,31 @@ return {
     end
   },
 
+  diff = {
+    'diff',
+    colored = true,
+    diff_color = {
+      -- Same color values as the general color option can be used here.
+      added    = 'DiffAdd',    -- Changes the diff's added color
+      modified = 'DiffChange', -- Changes the diff's modified color
+      removed  = 'DiffDelete', -- Changes the diff's removed color you
+    },
+    symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
+    source = nil, -- A function that works as a data source for diff.
+    -- It must return a table as such:
+    --   { added = add_count, modified = modified_count, removed = removed_count }
+    -- or nil on failure. count <= 0 won't be displayed.
+  },
+
+  fileformat = {
+    'fileformat',
+    symbols = {
+      unix = 'LF', -- e712
+      dos = 'CRLF',  -- e70f
+      mac = 'LF',  -- e711
+    }
+  },
+
   lsp = {
     function(msg)
       local buf_clients = vim.lsp.buf_get_clients()
