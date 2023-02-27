e6e3ec938b2bb6cfb52bc64c796bb7ab4160b3ca
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-27 17:25:07 -0500

    NvimTree revert removal of deprecated nvim-tree.config nvim_tree_callback

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 require "user.impatient"
 require "user.options"
