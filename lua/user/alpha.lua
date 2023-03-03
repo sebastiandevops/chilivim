5f587ae227094eeae19bf571f4694061426a19d5
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-03-03 07:28:57 -0500

    Removed unuse line

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,7 +5,6 @@ end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-  '                                                                   ',
   '          ,/                                                       ',
   '           (                                                       ',
   '          ( * /                                                    ',
