4003ab0bd3f59ac853e2a8dbd06a5d656dae9c8e
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-03-06 04:48:25 -0500

    deno comment in plugin/init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -67,5 +67,5 @@ return {
 	{ "nvim-treesitter/nvim-treesitter"	}, -- provide a simple and easy way to use the interface for tree-sitter in Neovim
 	{ "lewis6991/gitsigns.nvim" }, -- git decorations implemented purely in lua/teal
   { "nvim-treesitter/playground"}, -- View treesitter information directly in Neovim
-  { "toppair/peek.nvim", build='deno task --quiet build:fast' },
+  { "toppair/peek.nvim", build='deno task --quiet build:fast' }, -- Markdown live preview
 }
