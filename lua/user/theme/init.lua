f322303031f1b87e805c2fda968975fb08e07593
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-03-11 06:44:03 -0500

    theme

--- lua/user/theme/init.lua
+++ lua/user/theme/init.lua
@@ -1,18 +1,18 @@
 local utils = require("user.theme.utils")
 
-local colorscheme = "gruvbox-material"
+local colorscheme = "kanagawa"
 
 -- Some colorschemes have a strange behavior when loading configuration file.
 -- Most times, utility set_theme will load the configuration.
 -- if not just uncomment the require line. Make sure you have the config
 -- file under colorschemes folder.
 
--- require(colorscheme).setup(require("user.theme.colorschemes." .. colorscheme))
+require(colorscheme).setup(require("user.theme.colorschemes." .. colorscheme))
 
 utils.set_theme(colorscheme)
 
 -- Custom configuration
 vim.o.transparent = true
 vim.cmd "let &fcs='eob: '"
-vim.cmd "let g:gruvbox_material_transparent_background = 2"
-vim.cmd "let g:gruvbox_material_enable_bold = 1"
+-- vim.cmd "let g:gruvbox_material_transparent_background = 2"
+-- vim.cmd "let g:gruvbox_material_enable_bold = 1"
