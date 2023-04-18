05bee4d734134bf675ccf7236d9ec44163f53b0b
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-04-18 06:23:05 -0500

    Function on_attach for nvimtree implemented

--- lua/core/nvimtree.lua
+++ lua/core/nvimtree.lua
@@ -14,6 +14,27 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- Default mappings. Feel free to modify or remove as you wish.
+  api.config.mappings.default_on_attach(bufnr)
+  -- END_DEFAULT_ON_ATTACH
+
+  -- Mappings migrated from view.mappings.list
+  -- You will need to insert "your code goes here" for any mappings with a custom action_cb
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+
+end
+
 nvim_tree.setup {
   auto_reload_on_write = true,
   disable_netrw = false,
@@ -26,7 +47,7 @@ nvim_tree.setup {
   sync_root_with_cwd = false,
   reload_on_bufenter = false,
   respect_buf_cwd = false,
-  on_attach = "default",
+  on_attach = on_attach,
   remove_keymaps = false,
   select_prompts = false,
   update_focused_file = {
@@ -112,3 +133,5 @@ nvim_tree.setup {
     timeout = 400,
   },
 }
+
+
