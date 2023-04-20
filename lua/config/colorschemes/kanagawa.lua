504f85a416fd9fdf3825f24c0fd7340c36194392
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-04-20 08:27:45 -0500

    kanagawa updated

--- lua/config/colorschemes/kanagawa.lua
+++ lua/config/colorschemes/kanagawa.lua
@@ -23,12 +23,12 @@ return {
       -- Save an hlgroup with dark background and dimmed foreground
       -- so that you can use it where your still want darker windows.
       -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-      NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+      NormalDark = { fg = theme.ui.fg_dim, bg = "none" },
 
       -- Popular plugins that open floats will link to NormalFloat by default;
       -- set their background accordingly if you wish to keep them dark and borderless
-      LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-      MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+      LazyNormal = { bg = "none", fg = theme.ui.fg_dim },
+      MasonNormal = { bg = "none", fg = theme.ui.fg_dim },
     }
   end,
   theme = "dragon",              -- Load "wave" theme when 'background' option is not set
