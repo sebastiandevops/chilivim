c079d83e8695823c5d84af39d3f0dec16755ed3c
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-04-23 11:12:24 -0500

    Visual selection => #d4be98 according to gruvbox-material-theme

--- after/plugin/highlights.lua
+++ after/plugin/highlights.lua
@@ -19,7 +19,7 @@ au ColorScheme * hi NonText                             gui=bold            guif
 au ColorScheme * hi Whitespace                          gui=bold            guifg=#403d52  guibg=NONE
 " Selected area colour
 au ColorScheme * hi VisualNOS             ctermbg=NONE                                     guibg=NONE
-au ColorScheme * hi Visual                ctermbg=NONE                                     guibg=#6e6a86
+au ColorScheme * hi Visual                ctermbg=NONE                      guifg=#1d2021  guibg=#d4be98
 au ColorScheme * hi IndentBlanklineChar                 gui=bold            guifg=#403d52  guibg=NONE
 
 "Comments color
