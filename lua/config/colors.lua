903a89327544d697c13067c7d10dae7433aa1dc2
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-06-08 12:37:51 -0500

    vim.o.transparent deleted from colors.lua

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -8,7 +8,7 @@ local colorscheme = "horizon"
 utils.set_theme(colorscheme)
 
 -- Custom configuration
-vim.o.transparent = true
+-- vim.o.transparent = true
 vim.cmd "let &fcs='eob: '"
 vim.cmd "let g:gruvbox_material_transparent_background = 2"
 vim.cmd "let g:gruvbox_material_enable_bold = 1"
