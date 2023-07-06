3b3d5bbce213522dcf51404d1eb96c79e08d2db2
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-07-06 11:04:33 -0500

    after/plugin updated

--- after/plugin/highlights.lua
+++ after/plugin/highlights.lua
@@ -13,7 +13,7 @@ au ColorScheme * hi Delimiter                                               guif
 " Selected area colour
 " au ColorScheme * hi VisualNOS             ctermbg=NONE                                     guibg=NONE
 au ColorScheme * hi Visual                              gui=bold            guifg=#393552    guibg=#fcb2aa
-au ColorScheme * hi IndentBlanklineChar                 gui=bold            guifg=#56526e    guibg=NONE
+au ColorScheme * hi IndentBlanklineChar                 gui=bold            guifg=#66627e    guibg=NONE
 au ColorScheme * hi IndentBlanklineContextStart cterm=underline gui=underline guisp=#9893a5
 au ColorScheme * hi IndentBlanklineContextChar guifg=#9893a5
 " au ColorScheme * hi IndentBlanklineSpaceCharBlankline cterm=nocombine gui=nocombine guifg=#e0def4
