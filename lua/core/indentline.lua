29b35536aac2cb6b6742d5695e28e08188530cfb
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-09-15 20:24:33 -0500

    IndentBlankLine plugin updated

--- lua/core/indentline.lua
+++ lua/core/indentline.lua
@@ -6,7 +6,7 @@ end
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:¶"
-vim.opt.listchars:append "tab:» "
+vim.opt.listchars:append "tab:|⇢"
 vim.opt.listchars:append "trail:·"
 vim.opt.listchars:append "extends:>"
 vim.opt.listchars:append "precedes:<"
