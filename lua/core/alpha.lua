6a9992fb2645957a5e4fcf9cef08c27ec57dfd7f
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-09-24 07:52:11 -0500

    neorg: entry in alpha menu added

--- lua/core/alpha.lua
+++ lua/core/alpha.lua
@@ -33,7 +33,7 @@ dashboard.section.buttons.val = {
     dashboard.button("r",   "  Recently Used Files",         "<CMD>Telescope oldfiles<CR>" ),
     -- dashboard.button( { "SPC s",   "  Load last session",           "<CMD>SessionLoad<CR>" ),
     dashboard.button("t",   "  NvimTree",                    "<CMD>NvimTreeToggle<CR>" ),
-    dashboard.button("m",   "  Marks              ",         "<CMD>Telescope marks<CR>" ),
+    dashboard.button("N",   "  Neorg Index",                 "<CMD>Neorg index<CR>" ),
     dashboard.button("w",   "  Find Word",                   "<CMD>Telescope live_grep<CR>" ),
     dashboard.button("c",   "  Edit Configuration",          "<CMD>e ~/.config/nvim/lua/config/options.lua<CR>" ),
     dashboard.button("g",   "  Git status",                  "<CMD>Telescope git_status<CR>" ),
