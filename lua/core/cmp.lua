f6bfcd63ba681f243e8cc951391f54ae0278801b
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2024-01-30 10:02:35 -0500

    core/cmp: group_index parameter added to sources and sorting parameter added with custom prioritize for copilot

--- lua/core/cmp.lua
+++ lua/core/cmp.lua
@@ -146,14 +146,32 @@ cmp.setup {
     end,
   },
   sources = {
-    { name = "copilot" },
-    { name = "nvim_lsp" },
-    { name = "cmp_tabnine" },
-    { name = "nvim_lua" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-    { name = "neorg" },
+    { name = "copilot", group_index = 2 },
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "cmp_tabnine", group_index = 2 },
+    { name = "nvim_lua", group_index = 2 },
+    { name = "luasnip", group_index = 2 },
+    { name = "buffer", group_index = 2 },
+    { name = "path", group_index = 2 },
+    { name = "neorg", group_index = 2 },
+  },
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      require("copilot_cmp.comparators").prioritize,
+
+      -- Below is the default comparitor list and order for nvim-cmp
+      cmp.config.compare.offset,
+      -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.locality,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
